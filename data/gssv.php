bae203883d86d9f7b29cd098d1ab0680f22cf7e8
Jörg Bechtold <joergbechtold@gmx.net>
2025-03-25 13:23:29 +0100

    add first files

---
+++ data/gssv.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type" /><title>GSSV Hammelburg - Daniel Klüspies</title></head><body><table style="text-align: left; width: 270px; font-family: Times New Roman; font-weight: bold;" border="0" cellpadding="2" cellspacing="2"><tbody><tr><td style="font-weight: bold; font-size: large; font-family: Arial; color: rgb(54, 51, 46); line-height: 24pt;">Am Samstag den 08.06.2013 fällt die Gruppenübungsstunde um 14 Uhr, 15 Uhr und das THS-Training aus!<br />
+</td></tr></tbody></table></body></html>
---
+++ data/gssvtest2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type" /><title>GSSV Hammelburg - Daniel Klüspies</title></head><body><table style="text-align: left;width: 290px; font-family: Times New Roman; font-weight: bold;" border="0" cellpadding="2" cellspacing="2"><tbody><tr><td style="font-weight: bold; font-size: large; font-family: Arial; color: rgb(34, 34, 34); line-height: 24pt;">Am Samstag den 00.00.0000 fällt die Gruppenübungsstunde um 14 Uhr, 15 Uhr und das THS-Training aus!<br />
+</td></tr></tbody></table></body></html>
